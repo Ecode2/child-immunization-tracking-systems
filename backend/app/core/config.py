3ed5b67d5402c577fd0b59732e5d6d6e202e49c1
hallelx2 <halleluyaholudele@gmail.com>
2024-06-11 18:30:28 +0100

    changed the dotenv file to make it suitable for deployment

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,13 @@
+import os
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    DEFAULT_DATABASE: str
-    SECRET_KEY: str
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DEFAULT_DATABASE: str = os.getenv("DEFAULT_DATABASE")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
